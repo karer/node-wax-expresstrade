b7826edcc385618703863202b8c08bc7cbc2a05c
Kacper Geisheimer <devkarer@gmail.com>
2018-12-30 16:53:23 +0100

    Updated IUser interface

--- interfaces/IUser/index.js
+++ interfaces/IUser/index.js
@@ -21,14 +21,31 @@ class IUser extends ETInterface {
     return res.response
   }
 
-  async GetInventory({ appId, page = 1, perPage, search, sort }) {
+  async GetInventory({
+    appId,
+    page = 1,
+    perPage,
+    search,
+    sort,
+    filterInTrade
+  }) {
     if (appId === undefined) {
       throw new Error('appId not specified!')
     }
 
     const url = this.getUrl() + 'GetInventory/v1'
 
-    const res = await this.request.get({ url, qs: { app_id: appId, page, per_page: perPage, search, sort } })
+    const res = await this.request.get({
+      url,
+      qs: {
+        app_id: appId,
+        page,
+        per_page: perPage,
+        search,
+        sort,
+        filter_in_trade: filterInTrade
+      }
+    })
 
     if (!res.response) {
       throw new Error(res.message)
@@ -49,9 +66,21 @@ class IUser extends ETInterface {
     return res.response.user
   }
 
-  async UpdateProfile({ displayName, inventoryIsPrivate, allowTwoFactorCodeReuse }) {
+  async UpdateProfile({
+    displayName,
+    inventoryIsPrivate,
+    allowTwoFactorCodeReuse,
+    autoAcceptGiftTrades,
+    anonymousTransactions
+  }) {
     const url = this.getUrl() + 'UpdateProfile/v1'
-    const form = { display_name: displayName, inventory_is_private: inventoryIsPrivate, allow_twofactor_code_reuse: allowTwoFactorCodeReuse }
+    const form = {
+      display_name: displayName,
+      inventory_is_private: inventoryIsPrivate,
+      allow_twofactor_code_reuse: allowTwoFactorCodeReuse,
+      auto_accept_gift_trades: autoAcceptGiftTrades,
+      anonymous_transactions: anonymousTransactions
+    }
     const res = await this.request.post({ url, form })
 
     if (!res.response) {
@@ -59,6 +88,32 @@ class IUser extends ETInterface {
     }
 
     return res.response.user
+  }
+
+  async UserReports({ message, reportType, offerId }) {
+    if (message === undefined) {
+      throw new Error('message not specified!')
+    }
+    if (reportType === undefined) {
+      throw new Error('reportType not specified!')
+    }
+    if (offerId === undefined) {
+      throw new Error('offerId not specified!')
+    }
+
+    const url = this.getUrl() + 'UserReports/v1'
+    const form = {
+      message,
+      report_type: reportType,
+      offer_id: offerId
+    }
+    const res = await this.request.post({ url, form })
+
+    if (res.response === undefined) {
+      throw new Error(res.message)
+    }
+
+    return res.response
   }
 
   getUrl() {
